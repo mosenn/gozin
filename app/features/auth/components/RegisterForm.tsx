888a23bbaf129595f86300bf9bb828506e7cfb6b
amir-es85 <amirfname@gmail.com>
2026-09-01 22:20:45 +0330

    add shadcn

--- app/features/auth/components/RegisterForm.tsx
+++ app/features/auth/components/RegisterForm.tsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod";
 import { RegisterFormValues } from "../types/auth.types";
 import { registerSchema } from "../schemas/RegisterSchema";
+import { Input } from "@/components/ui/Input";
 
 
 function Registerform() {
@@ -16,7 +17,9 @@ function Registerform() {
   return (
 <Container>
     <form onSubmit={handleSubmit(onSubmit)}>
-
+<Input placeholder="نام خود را وارد کنید" {...register("name")}/>
+<Input placeholder="ایمیل خود را وارد کنید" {...register("email")}/>
+<Input placeholder="رمز خود را وارد کنید" {...register("password")}/>
     </form>
 </Container>
     
