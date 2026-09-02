dfab2ce899bb0f4632047dff9faa45aa10de7315
amir-es85 <amirfname@gmail.com>
2026-09-03 01:48:25 +0330

    debug register form

--- app/features/auth/components/RegisterForm.tsx
+++ app/features/auth/components/RegisterForm.tsx
@@ -6,24 +6,30 @@ import { registerSchema } from "../schemas/RegisterSchema";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import { registerAction } from "../services/RegisterAction";
+import { useRouter } from "next/navigation";
 
 
 function Registerform() {
+    const router = useRouter();
+
     const {register,handleSubmit,reset,formState}=useForm<RegisterFormValues>({
         resolver:zodResolver(registerSchema)
     })
 
    async function onSubmit(formdata:RegisterFormValues) {
     const result = await registerAction(formdata);
+    if (result.success) {
+    router.push("/login");
+  }
     
    }
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-<Input placeholder="نام خود را وارد کنید" {...register("name")}/>
-<Input placeholder="ایمیل خود را وارد کنید" {...register("email")}/>
+        <Input placeholder="ایمیل خود را وارد کنید" {...register("email")}/>
 <Input placeholder="رمز خود را وارد کنید" {...register("password")}/>
-<Button disabled={formState.isSubmitting || !formState.isValid}>
+<Input type="number" placeholder="سن خود را وارد کنید" {...register("age", { valueAsNumber: true })}/>
+<Button type="submit" disabled={formState.isSubmitting || !formState.isValid}>
     {formState.isSubmitting?"در حال ثبت نام":"ثبت نام"}
 </Button>
     </form>
--- app/features/auth/services/RegisterAction.ts
+++ app/features/auth/services/RegisterAction.ts
@@ -3,32 +3,35 @@
 import { RegisterFormValues } from "../types/auth.types";
 
 export async function registerAction(data: RegisterFormValues) {
+  
+  const url = `${process.env.NEXT_PUBLIC_API_URL}/auth/register`;
   try {
-    const response = await fetch(
-      `${process.env.API_URL}/auth/register`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      }
-    );
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
 
     const result = await response.json();
 
     if (!response.ok) {
+
       return {
         success: false,
         message: result.message || "ثبت نام ناموفق بود",
       };
     }
 
+    
+
     return {
       success: true,
-      message:`کاربر ${data.name}، ثبت نام شما با موفقیت انجام شد`,
+      message: "ثبت نام شما با موفقیت انجام شد",
     };
-  } catch {
+  } catch (error) {
+
     return {
       success: false,
       message: "خطایی در ارتباط با سرور رخ داد",
